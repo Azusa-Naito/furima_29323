d8227d513594bda659939dd4d00ab7f6faa93b19
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-09-09 14:38:05 +0900

    修正1
    1.コメント削除
    2.スペース挿入（他の箇所と合わせるため）

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,9 +1,2 @@
 module ItemsHelper
-  # def item_list(items)
-  #   html = ''
-  #   items.each do |item|
-  #     html += render(partial: 'item', locals{ item: item })
-  #   end
-  # return raw(html)
-  # end
 end
