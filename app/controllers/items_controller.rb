a5a2e8c3de7faa1b20e57c72b7117cd7399e661a
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-09-11 18:59:09 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    end
   end
 
   private
