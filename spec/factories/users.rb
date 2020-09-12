322e811508cdf09d7f6b1bcc95bcabc677206464
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-09-12 18:22:25 +0900

    修正
    Rubocopによるインデント 修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { '1a' + Faker::Internet.password(min_length: 4)}
-    password_confirmation {password}
-    last_name             {Gimei.last.kanji}
-    first_name            {Gimei.first.kanji}
-    last_name_k           {Gimei.last.katakana}
-    first_name_k          {Gimei.first.katakana}
-    birthday              {Faker::Date.birthday(min_age: 12, max_age: 100)}
+    password              { '1a' + Faker::Internet.password(min_length: 4) }
+    password_confirmation { password }
+    last_name             { Gimei.last.kanji }
+    first_name            { Gimei.first.kanji }
+    last_name_k           { Gimei.last.katakana }
+    first_name_k          { Gimei.first.katakana }
+    birthday              { Faker::Date.birthday(min_age: 12, max_age: 100) }
   end
 end
