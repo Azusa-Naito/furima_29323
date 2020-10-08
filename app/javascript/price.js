0988656424dc59e0c887c566cd988bb7370f2492
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-10-08 22:45:16 +0900

    JavaScriptのイベント発火ページの設定

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,18 +1,20 @@
-function price() {
-  const inputPrice = document.getElementById("item-price");
-  const displayedFee = document.getElementById("add-tax-price");
-  const displayedProfit = document.getElementById("profit");
-  
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  function price() {
+    const inputPrice = document.getElementById("item-price");
+    const displayedFee = document.getElementById("add-tax-price");
+    const displayedProfit = document.getElementById("profit");
+    
 
-  inputPrice.addEventListener( 'input', () => {
-    const inputValue = inputPrice.value;
-    if ( inputValue >=300 && inputValue <= 9999999 ) {
-    displayedFee.textContent = Math.floor(inputValue * 0.1);
-    displayedProfit.textContent = inputValue - Math.floor(inputValue * 0.1);
-    } else {
-      displayedFee.textContent = " -- ";
-      displayedProfit.textContent = " -- ";
-    }
-  });
+    inputPrice.addEventListener( 'input', () => {
+      const inputValue = inputPrice.value;
+      if ( inputValue >=300 && inputValue <= 9999999 ) {
+      displayedFee.textContent = Math.floor(inputValue * 0.1);
+      displayedProfit.textContent = inputValue - Math.floor(inputValue * 0.1);
+      } else {
+        displayedFee.textContent = " -- ";
+        displayedProfit.textContent = " -- ";
+      }
+    });
+  }
+  window.addEventListener('load', price)
 }
-window.addEventListener('load', price)
