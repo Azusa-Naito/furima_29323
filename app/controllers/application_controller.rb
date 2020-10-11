59cf12de7b33100b13e59aa3039f22afb8414e41
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-10-11 21:18:41 +0900

    ActiveSupport::LoggerThreadSafeLevel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  class MyLogger < Logger
+    include LoggerSilence
+    include ActiveSupport::LoggerThreadSafeLevel
+  end
+  
+  before_action :mylogger_test
 
   private
+
+  def mylogger_test
+    mylogger = MyLogger.new(STDOUT)
+    mylogger.silence do
+      mylogger.debug("controller = #{controller_name}")
+      mylogger.info("action = #{action_name}")
+      mylogger.error("controler#action = #{controller_name}##{action_name}")
+    end
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
